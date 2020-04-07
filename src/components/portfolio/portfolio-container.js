7466e041e36d24d6f6712fc3e48d36cb9e29c510
wjosephmark <wjosephmark@gmail.com>
2020-04-07 13:41:29 -0600

    Added change title button

--- src/components/portfolio/portfolio-container.js
+++ src/components/portfolio/portfolio-container.js
@@ -8,23 +8,40 @@ export default class PortfolioContainer extends Component {
     constructor() {
         super()
 
-        console.log("Portfolio container has rendered")
+        this.state = {
+            pageTitle: "Welcome to my portfolio",
+            data: [
+                {title: "Quip"},
+                {title: "Eventbrite"},
+                {title: "Ministry Safe"}
+            ]
+        }
+
+        this.handlePageTitleUpdate.bind(this)
     }
 
     portfolioItems(){
-        const data = ["Quip", "Eventbrite", "Ministry Safe"]
+        return this.state.data.map(item => {
+            return <PortfolioItem title ={item.title} url={"google.com"} />
+        })
+    }
 
-        return data.map(item => {
-            return <PortfolioItem title ={item} url={"google.com"} />
+    handlePageTitleUpdate() {
+        this.setState({
+            pageTitle: "Something Else",
         })
     }
 
     render(){
         return (
             <div>
-                <h2>Portfolio items go here, update...</h2>
+                <h2>{this.state.pageTitle}</h2>
 
                 {this.portfolioItems()}
+
+                <hr />
+
+                <button onClick={this.handlePageTitleUpdate}>Change Title</button>
             </div>
         )
     }
--- src/components/portfolio/portfolio-item.js
+++ src/components/portfolio/portfolio-item.js
@@ -3,7 +3,8 @@ import React from 'react'
 export default function(props) {
     return(
         <div>
-            <h3>{props.title}</h3> <h4>{props.url}</h4>
+            <h3>{props.title}</h3> 
+            <h4>{props.url}</h4>
         </div>
     )
 }
