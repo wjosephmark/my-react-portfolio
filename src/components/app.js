631372edf6bc4595e22512439b3836ccf0c948b0
wjosephmark <wjosephmark@gmail.com>
2020-04-15 15:39:31 -0600

    Added security feature to blog

--- src/components/app.js
+++ src/components/app.js
@@ -1,40 +1,109 @@
-import React, { Component } from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import axios from "axios"
 
-import PortfolioContainer from "./portfolio/portfolio-container"
-import NavigtationContainer from "./navigation/navigation-container"
-import Home from "./pages/home"
-import About from "./pages/about"
-import Contact from "./pages/contact"
-import Blog from "./pages/blog"
-import PortfolioDetail from "./portfolio/portfolio-detail"
-import NoMatch from "./pages/no-match"
-import axios from 'axios';
+import NavigationContainer from "./navigation/navigation-container";
+import Home from "./pages/home";
+import About from "./pages/about";
+import Contact from "./pages/contact";
+import Blog from "./pages/blog";
+import PortfolioDetail from "./portfolio/portfolio-detail";
+import Auth from "./pages/auth";
+import NoMatch from "./pages/no-match";
 
 export default class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      loggedInStatus: "NOT_LOGGED_IN"
+    };
+
+    this.handleSuccessfulLogin = this.handleSuccessfulLogin.bind(this);
+    this.handleUnsuccessfulLogin = this.handleUnsuccessfulLogin.bind(this);
+  }
+
+  handleSuccessfulLogin() {
+    this.setState({
+      loggedInStatus: "LOGGED_IN"
+    });
+  }
+
+  handleUnsuccessfulLogin() {
+    this.setState({
+      loggedInStatus: "NOT_LOGGED_IN"
+    });
+  }
+
+  checkLoginStatus(){
+    return axios.get("https://api.devcamp.space/logged_in", {
+      withCredentials: true
+    }).then(response => {
+      const loggedIn = response.data.logged_in
+      const loggedInStatus = this.state.loggedInStatus;
+
+      if(loggedIn && loggedInStatus === "LOGGED_IN") {
+        return loggedIn
+      } else if (loggedIn && loggedInStatus === "NOT_LOGGED_IN"){
+        this.setState({
+          loggedInStatus: "LOGGED_IN"
+        })
+      } else if (!loggedIn && loggedInStatus === "LOGGED_IN"){
+        this.setState({
+          loggedInStatus: "NOT_LOGGED_IN"
+        })
+      }
+    })
+    .catch(error => {
+
+    })
+  }
+
+  componentDidMount(){
+    this.checkLoginStatus()
+  }
+
+  authorizedPages() {
+    return [
+      <Route path="/blog" component={Blog} />
+    ]
+  }
+
   render() {
-    
     return (
-      <div className='container'>
-
-        <h1>Joseph Mark Portfolio</h1>
-
+      <div className="container">
         <Router>
           <div>
-            <NavigtationContainer />
-            {/* <PortfolioContainer /> */}
+            <NavigationContainer loggedInStatus={this.state.loggedInStatus}/>
+
+            <h2>{this.state.loggedInStatus}</h2>
 
             <Switch>
-              <Route exact path ='/' component={Home} />
-              <Route path ='/about-me' component={About} />
-              <Route path ='/blog' component={Blog} />
-              <Route path ='/contact' component={Contact} />
-              <Route path ="/portfolio/:slug" component={PortfolioDetail} />
+              <Route exact path="/" component={Home} />
+
+              <Route
+                path="/auth"
+                render={props => (
+                  <Auth
+                    {...props}
+                    handleSuccessfulLogin={this.handleSuccessfulLogin}
+                    handleUnsuccessfulLogin={this.handleUnsuccessfulLogin}
+                  />
+                )}
+              />
+
+              <Route path="/about-me" component={About} />
+              <Route path="/contact" component={Contact} />
+              {this.state.loggedInStatus === "LOGGED_IN" ? this.authorizedPages() : null}
+              <Route
+                exact
+                path="/portfolio/:slug"
+                component={PortfolioDetail}
+              />
               <Route component={NoMatch} />
             </Switch>
           </div>
         </Router>
-
       </div>
     );
   }
--- src/components/portfolio/portfolio-item.js
+++ src/components/portfolio/portfolio-item.js
@@ -1,24 +1,45 @@
-import React from "react";
+import React, {Component} from "react";
 import { Link } from "react-router-dom";
 
-export default function(props) {
-  const { id, description, thumb_image_url, logo_url } = props.item;
-  return (
-    <div className="portfolio-item-wrapper">
-      <div
-        className="portfolio-img-background"
-        style={{
-          backgroundImage: "url(" + thumb_image_url + ")"
-        }}
-      />
+export default class PortfolioItem extends Component {
+    constructor(props){
+        super(props)
 
-      <div className="img-text-wrapper">
-        <div className="logo-wrapper">
-          <img src={logo_url} />
-        </div>
+        this.state = {
+            portfolioItemClass: " "
+        }
+    }
 
-        <div className="subtitle">{description}</div>
-      </div>
-    </div>
-  );
+    handleMouseEnter() {
+        this.setState({portfolioItemClass: 'image-blur'})
+    }
+
+    handleMouseLeave() {
+        this.setState({portfolioItemClass: ''})
+    }
+
+    render() {
+        const { id, description, thumb_image_url, logo_url } = this.props.item;
+        return (
+            <div className="portfolio-item-wrapper"
+            onMouseEnter ={() => {this.handleMouseEnter()}}
+            onMouseLeave ={() => {this.handleMouseLeave()}}
+            >
+            <div
+                className={"portfolio-img-background " + this.state.portfolioItemClass}
+                style={{
+                backgroundImage: "url(" + thumb_image_url + ")"
+                }}
+            />
+
+            <div className="img-text-wrapper">
+                <div className="logo-wrapper">
+                <img src={logo_url} />
+                </div>
+
+                <div className="subtitle">{description}</div>
+            </div>
+            </div>
+        );
+    }
 }
