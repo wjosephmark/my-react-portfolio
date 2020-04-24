4132259b0edc0ad298c9dd49143d6d805dafb042
wjosephmark <wjosephmark@gmail.com>
2020-04-24 15:31:26 -0600

    fixed memory leak:

--- src/components/Pages/blog.js
+++ src/components/Pages/blog.js
@@ -16,20 +16,20 @@ class Blog extends Component {
         }
 
         this.getBlogItems = this.getBlogItems.bind(this)
-        this.activateInfiniteScroll()
+        this.onScroll = this.onScroll.bind(this)
+        window.addEventListener("scroll", this.onScroll, false)
     }
 
-    activateInfiniteScroll(){
-        window.onscroll = () => {
-            if(this.state.blogItems.length === this.state.totalCount) {
-                return
-            }
+    onScroll(){
+        if(this.state.isLoading || this.state.blogItems.length === this.state.totalCount) {
+            return
+        }
 
-            if (window.innerHeight + document.documentElement.scrollTop === document.documentElement.offsetHeight) {
-                this.getBlogItems()
-            }
+        if (window.innerHeight + document.documentElement.scrollTop === document.documentElement.offsetHeight) {
+            this.getBlogItems()
         }
     }
+
 
     getBlogItems() {
         this.setState({
@@ -51,6 +51,10 @@ class Blog extends Component {
 
     componentWillMount(){
         this.getBlogItems()
+    }
+
+    componentWillUnmount(){
+        window.removeEventListener("scroll", this.onScroll, false)
     }
 
     render(){
