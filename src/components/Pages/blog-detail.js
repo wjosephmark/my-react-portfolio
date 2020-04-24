d599f0e06b0db3f6fa33c929047c43e41a5f305c
wjosephmark <wjosephmark@gmail.com>
2020-04-24 10:05:27 -0600

    deconstructed blogItem

--- src/components/Pages/blog-detail.js
+++ src/components/Pages/blog-detail.js
@@ -16,6 +16,9 @@ export default class BlogDetail extends Component {
         .currentId}`
         ).then(response => {
             console.log("response", response)
+            this.setState({
+                blogItem: response.data.portfolio_blog
+            })
         }).catch(error => {
             console.log("getBlogItem error", error)
         })
@@ -26,10 +29,20 @@ export default class BlogDetail extends Component {
     }
 
     render () {
-        console.log("currentId", this.state.currentId)
+        const {
+            title,
+            content,
+            featured_image_url,
+            blog_status
+        } = this.state.blogItem
+
         return (
             <div>
-                <h1>Blog detail</h1>
+                <h1>{title}</h1>
+                <img src={featured_image_url} />
+                <div>
+                    {content}
+                </div>
             </div>
         )
     }
