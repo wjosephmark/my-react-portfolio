b82733d6d0aa9986fc095fe08525dbc3ba9d9fef
wjosephmark <wjosephmark@gmail.com>
2020-04-24 16:53:52 -0600

    added modal close ability

--- src/components/Pages/blog.js
+++ src/components/Pages/blog.js
@@ -21,6 +21,13 @@ class Blog extends Component {
         this.onScroll = this.onScroll.bind(this)
         window.addEventListener("scroll", this.onScroll, false)
         this.handleNewBlogClick = this.handleNewBlogClick.bind(this)
+        this.handleModalClose = this.handleModalClose.bind(this)
+    }
+
+    handleModalClose() {
+        this.setState({
+            blogModalIsOpen: false
+        })
     }
 
     handleNewBlogClick() {
@@ -72,7 +79,10 @@ class Blog extends Component {
         })
         return (
             <div className="blog-container">
-                <BlogModal modalIsOpen={this.state.blogModalIsOpen}/>
+                <BlogModal 
+                modalIsOpen={this.state.blogModalIsOpen}
+                handleModalClose={this.handleModalClose}
+                />
 
                 <div className="new-blog-link">
                     <a onClick={this.handleNewBlogClick}>
