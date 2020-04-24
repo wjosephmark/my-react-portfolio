bd98c95e24d8766a850ec4903351eefc9f9f5fbe
wjosephmark <wjosephmark@gmail.com>
2020-04-24 09:48:43 -0600

    added getBlogItem function

---
+++ src/components/Pages/blog-detail.js
@@ -0,0 +1,36 @@
+import React, {Component} from 'react'
+import axios from "axios"
+
+export default class BlogDetail extends Component {
+    constructor(props){
+        super(props)
+
+        this.state ={
+            currentId: this.props.match.params.slug,
+            blogItem: {}
+        }
+    }
+
+    getBlogItem() {
+        axios.get(`https://josephm.devcamp.space/portfolio/portfolio_blogs/${this.state
+        .currentId}`
+        ).then(response => {
+            console.log("response", response)
+        }).catch(error => {
+            console.log("getBlogItem error", error)
+        })
+    }
+
+    componentDidMount(){
+        this.getBlogItem()
+    }
+
+    render () {
+        console.log("currentId", this.state.currentId)
+        return (
+            <div>
+                <h1>Blog detail</h1>
+            </div>
+        )
+    }
+}
