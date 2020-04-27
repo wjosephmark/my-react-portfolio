68fd7b069e171ecb84ccc2f773a46d59e2c038c2
wjosephmark <wjosephmark@gmail.com>
2020-04-27 16:58:39 -0600

    added wysiwyg to modal and stlyed

--- src/components/blog/blog-form.js
+++ src/components/blog/blog-form.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import axios from "axios";
+import RichTextEditor from "../forms/rich-text-editor"
 
 export default class BlogForm extends Component {
   constructor(props) {
@@ -71,7 +72,9 @@ export default class BlogForm extends Component {
             value={this.state.blog_status}
           />
         </div>
-
+        <div className="one-column">
+          <RichTextEditor />
+        </div>
         <button className="btn">Save</button>
       </form>
     );
