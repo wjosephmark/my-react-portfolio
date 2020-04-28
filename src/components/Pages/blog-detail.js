decfaacd6721554aaa3a6d7a3f47176c3b1671f9
wjosephmark <wjosephmark@gmail.com>
2020-04-28 14:33:43 -0600

    added Truncate feature to blog

--- src/components/Pages/blog-detail.js
+++ src/components/Pages/blog-detail.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react'
 import axios from "axios"
 import ReactHtmlParser from "react-html-parser"
+import BlogFeaturedImage from '../blog/blog-featured-image'
 
 export default class BlogDetail extends Component {
     constructor(props){
@@ -41,6 +42,8 @@ export default class BlogDetail extends Component {
             <div className="blog-container">
                 <div className="content-container">
                     <h1>{title}</h1>
+
+                    <BlogFeaturedImage img={featured_image_url} />
 
                     <div className="content">{ReactHtmlParser(content)}</div>
                 </div>
