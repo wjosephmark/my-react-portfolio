6d7e7c89ca71b66134ee6a6063c796fd7563b718
wjosephmark <wjosephmark@gmail.com>
2020-04-28 20:56:52 -0600

    pulled in image to react dropzone when editing

--- src/components/blog/blog-form.js
+++ src/components/blog/blog-form.js
@@ -135,18 +135,36 @@ export default class BlogForm extends Component {
         <div className="one-column">
           <RichTextEditor 
           handleRichTextEditorChange={this.handleRichTextEditorChange}
+          editMode={this.props.editMode}
+          contentToEdit={
+            this.props.editMode && this.props.blog.content 
+              ? this.props.blog.content
+              : null
+            }
           />
         </div>
 
         <div className="image-uploaders">
+        {this.props.editMode && this.props.blog.featured_image_url ? (
+            <div className="portfolio-manager-image-wrapper">
+            <img src={this.props.blog.featured_image_url} />
+
+            <div className="image-removal-link">
+              <a>
+                Remove file
+              </a>
+            </div>
+          </div>
+          ) : (
           <DropzoneComponent
-          ref={this.featuredImageRef}
+            ref={this.featuredImageRef}
             config={this.componentConfig()}
             djsConfig={this.djsConfig()}
             eventHandlers={this.handleFeaturedImageDrop()}
           >
           <div className="dz-message">Featured Image</div>
           </DropzoneComponent>
+          )}
         </div>
 
         <button className="btn">Save</button>
