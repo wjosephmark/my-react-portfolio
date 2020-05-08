97540fe674937ec23c5c4fb6529f9d0a4e6a8e24
wjosephmark <wjosephmark@gmail.com>
2020-05-08 09:43:14 -0600

    added styling to tool-manager

--- src/components/app.js
+++ src/components/app.js
@@ -94,6 +94,7 @@ export default class App extends Component {
             />
 
             <Switch>
+              
               <Route exact path="/" component={Home} />
 
               <Route
