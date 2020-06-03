9405ec57d1328417831768468bf87d291b94ec6b
wjosephmark <wjosephmark@gmail.com>
2020-06-02 22:48:40 -0600

    changed styling on home page:

--- src/components/Pages/home.js
+++ src/components/Pages/home.js
@@ -4,7 +4,7 @@ import PortfolioContainer from "../portfolio/portfolio-container"
 export default function() {
     return (
         <div>
-            <h2>Home page</h2>
+            <h2>Home page.</h2>
             <PortfolioContainer />
         </div>
     )
